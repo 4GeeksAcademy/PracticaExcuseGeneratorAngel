85ff821110213d65500c138de08b2df1cfd8870d
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2023-11-15 20:22:05 +0000

    primero

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generadorexcusas(who, action, what, when) {
+    let whorandom = Math.floor(Math.random() * who.length);
+    let actionrandom = Math.floor(Math.random() * action.length);
+    let whatrandom = Math.floor(Math.random() * what.length);
+    let whenrandom = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whorandom] +
+      " " +
+      action[actionrandom] +
+      " " +
+      what[whatrandom] +
+      " " +
+      when[whenrandom]
+    );
+  }
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let fraserandom = generadorexcusas(who, action, what, when);
+  document.querySelector("#excuse").innerHTML = fraserandom;
 };
